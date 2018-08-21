e44fd23230822226a95e4be664610fe58a135fe5
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-08-20 22:16:09 -0500

    added routings to Login and MyCategories

---
+++ src/components/MyCategories/MyCategories.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './MyCategories.css';
+
+class MyCategories extends React.Component {
+  render () {
+    return (
+      <div className="MyCategories">
+        <h2>My Categories</h2>
+      </div>
+    );
+  };
+};
+
+export default MyCategories;
