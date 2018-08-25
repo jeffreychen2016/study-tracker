d451e71d45d066b850127eb5187a427e21ac6256
Jeffrey Chen <jeffreychen2016@gmail.com>
2018-08-25 13:25:12 -0500

    added TimeClock component files, and added digital clock

---
+++ src/components/TimeClock/TimeClock.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import './TimeClock.css';
+
+class TimeClock extends React.Component {
+  state = {
+    time: "00:00:00",
+    amPm: "am"
+  }
+  
+  componentDidMount () {
+    this.loadInterval = setInterval(
+      this.getTime, 0
+    );
+  }
+  
+  getTime = () => { 
+    const takeTwelve = n => n > 12 ? n  - 12 : n;
+    const addZero = n => n < 10 ? "0" +  n : n;
+       
+    setInterval(() => {
+      let d, h, m, s, t, amPm;
+      
+      d = new Date();
+      h = addZero(takeTwelve(d.getHours())); 
+      m = addZero(d.getMinutes()); 
+      s = addZero(d.getSeconds());
+      t = `${h}:${m}:${s}`;
+
+      amPm = d.getHours() >= 12 ? "pm" : "am";
+
+      this.setState({
+        time: t, 
+        amPm: amPm
+      });
+      
+    }, 1000);
+  }
+  
+  render () {
+    return (
+      <div className="TimeClock">
+        <div className="outer">
+          <div className="inner">
+            <div className="most-inner">
+              <span className={
+                this.state.time === "00:00:00" 
+                  ? "time blink" 
+                  : "time"} 
+              > {this.state.time}
+              </span>
+              <span className="amPm">
+                {this.state.amPm}
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+};
+
+export default TimeClock;
